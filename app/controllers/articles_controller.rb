82fcff170fdc4adc711eaf0dea8fbd7835ebb376
tu77919 <denny.r.heath@gmail.com>
2018-11-08 22:33:16 -0500

    Add association between user and articles and add debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,8 +12,10 @@ class ArticlesController < ApplicationController
   end
   
   def create
+    #debugger  If need on the fly debugging - un comment debugger and you can utilize the server win like rails console
     #render plain: params[:article].inspect  -- This was a first step to see what happened to the params created in the new form (Not needed, but good to know)
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
